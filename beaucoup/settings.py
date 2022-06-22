f6ac7d991482d6b400cd3df0cc6d9ee0fe1b65c8
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-06-22 08:56:06 +0300

    ft:register home template

--- beaucoup/settings.py
+++ beaucoup/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config,Csv
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
